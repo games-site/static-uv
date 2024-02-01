c350a892f2863f2e07468ea52b335e1b51f31a0c
JoshXD07 <134786420+games-site@users.noreply.github.com>
2024-02-01 20:12:54 +1100

    commit 4

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,7 +1,7 @@
 // This file overwrites the stock UV config.js
 
 self.__uv$config = {
-  prefix: "/static-uv/uv/service/",
+  prefix: "",
   bare: "https://uv.holyubofficial.net/",
   encodeUrl: Ultraviolet.codec.xor.encode,
   decodeUrl: Ultraviolet.codec.xor.decode,
